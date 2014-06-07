0f8ffdd5b390a117ffb1efdfb90a8aa9a88107f6
Stephen Zhou <zhoufwind@hotmail.com>
2014-06-06 22:17:47 -0700

    Update: we are store multiple names at one time!

--- py_paramMyNames/py_paramMyNames.py
+++ py_paramMyNames/py_paramMyNames.py
@@ -8,16 +8,17 @@ def init(data):
 def lookup(data, label, name):
     return data[label].get(name)
 
-def store(data, full_name):
-    names = full_name.split()
-    if len(names) == 2: names.insert(1, '')
-    labels = 'first', 'middle', 'last'
-    for label, name in zip(labels, names):
-        people = lookup(data, label, name)
-        if people:
-            people.append(full_name)
-        else:
-            data[label][name] = [full_name]
+def store(data, *full_names):
+    for full_name in full_names:
+        names = full_name.split()
+        if len(names) == 2: names.insert(1, '')
+        labels = 'first', 'middle', 'last'
+        for label, name in zip(labels, names):
+            people = lookup(data, label, name)
+            if people:
+                people.append(full_name)
+            else:
+                data[label][name] = [full_name]
 
 MyNames = {}
 init(MyNames)
@@ -33,3 +34,7 @@ print lookup(MyNames, 'first', 'Robin')
 store(MyNames, 'Mr. Gumby')
 lookup(MyNames, 'middle', '')
 print lookup(MyNames, 'middle', '')
+
+store(MyNames, 'Luke Skywalker', 'Anakin Skywalker')
+lookup(MyNames, 'last', 'Skywalker')
+print lookup(MyNames, 'last', 'Skywalker')
